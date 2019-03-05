3e2442d55cb0b9c1e9e0f8d7116d86943daf885e
Habeeb Okunade <guessy2k2@yahoo.com>
2019-03-06 09:20:44 +1100

    frameset

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
     //welcome to emeat - Australia
 ?>
 
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<!--<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">-->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
+
 <html>
 <head>
 <title>eMeat Australia - Order Meat Online</title>
